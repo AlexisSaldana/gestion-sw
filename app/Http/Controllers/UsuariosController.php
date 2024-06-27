1e0a98beacb328645b5a1feef3f3f0a8e9e89be9
Alexis Saldaña Carvajal <2130134@upv.edu.mx>
2024-06-27 00:09:02 -0600

    Version mas organizada

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Paciente;
 
 class UsuariosController extends Controller
 {
